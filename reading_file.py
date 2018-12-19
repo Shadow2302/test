d6f2b4a194fdde5f13885fcc5eb70abe338e99ec
Shadow <shadow2302ezg@gmail.com>
2018-12-19 19:03:39 +0100

    commit lellel

--- reading_file.py
+++ reading_file.py
@@ -1,8 +1,4 @@
-stats = {
-    "Alex": 0,
-    "Bob": 0,
-    "Brex": 0
-}
+stats = {}
 
 errors = 0
 
@@ -11,17 +7,47 @@ def print_error():
     print("ti tupoi?")
 
 
+def smart_int(string):
+    new_string = ""
+    for symbol in string:
+        if symbol >= '0' and symbol <= '9':
+            new_string += symbol
+        else:
+            pass
+    return int(new_string)
+
+
+def smart_int_display(num):
+    string = str(num)
+    new_string = ""
+    i = 1
+    for sym in string[::-1]:
+        if i == 3:
+            new_string += sym + " "
+            i = 1
+        else:
+            new_string += sym
+            i += 1
+    return new_string[::-1]
+
+
 with open("text.txt", "r", encoding="utf-8") as file:
     for line in file:
         line = line.strip()
         name, points = line.split(":")
+        stats.setdefault(name, 0)
         try:
-            stats[name] += int(points)
+            stats[name] += smart_int(points)
         except ValueError:
             print_error()
             errors += 1
 
-winner = max(stats)
+winner = ""
+total = 0
+for name, points in stats.items():
+    if points > total:
+        total = points
+        winner = name
 
 print("The winner is {} with {} points.\n{} errors occured.".format(
-    winner, stats[winner], errors))
+    winner, smart_int_display(total), errors))
